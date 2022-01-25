32a32397de0e76e4921df51fa1f1ac63df8debaf
adamchappell00 <adam.chappell00@gmail.com>
2022-01-25 09:06:21 -0600

    DB Exercise ready for test

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,8 +4,8 @@ CREATE TABLE albums (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         artist VARCHAR(50) NOT NULL,
         name VARCHAR(50) NOT NULL,
-        release_date INT NOT NULL,
-        sales FLOAT,
+        release_date INT NOT NULL DEFAULT 'UNK',
+        sales FLOAT UNSIGNED,
         genre VARCHAR(50),
         PRIMARY KEY (id)
 );
