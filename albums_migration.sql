9c66c9e4375a810b26ae03c35ed1aefe2272df39
adamchappell00 <adam.chappell00@gmail.com>
2022-01-25 09:14:40 -0600

    db created, tested, refactored for defaults

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums (
+CREATE TABLE IF NOT EXISTS albums (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         artist VARCHAR(50) NOT NULL,
         name VARCHAR(50) NOT NULL,
-        release_date INT NOT NULL DEFAULT 'UNK',
+        release_date INT NOT NULL DEFAULT '0000',
         sales FLOAT UNSIGNED,
         genre VARCHAR(50),
         PRIMARY KEY (id)
